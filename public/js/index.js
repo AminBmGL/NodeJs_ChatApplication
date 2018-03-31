5828370bbce073346c895447e698b7fe89ea2d47
Amin Bm <medaminbenmbarek@gmail.com>
2018-03-31 17:10:14 +0100

    create simple message events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket =io();
+socket.on('connect',()=>{
+    console.log('Connected to the server');
+
+    socket.emit('createMessage',{
+       from:"amin@gmail.com",
+       text:'Hello !'
+    });
+
+});
+
+socket.on('newMessage',function (message) {
+    console.log('new message comes',message);
+});
+
+socket.on('disconnect',()=>{
+    console.log('Disconnected from the server');
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 const path =require('path');
 const publicPath =path.join(__dirname,'../public');
+const http =require('http');
 const express =require('express');
+const socketIO =require('socket.io');
+
 
 var port =process.env.PORT || 3000;
 var app =express();
+var server= http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,() =>{
+io.on('connection',(socket)=>{
+    console.log("A new client id connected");
+
+    socket.emit('newMessage',{
+from:'amin@gmail.com',
+        text :"hello everybody",
+        createdAt :123
+    });
+
+    socket.on('createMessage',(message)=>{
+       console.log('Email created',message)
+    });
+
+    socket.on('disconnect',()=>{
+    console.log('The client was disconnected');
+    });
+});
+
+
+server.listen(port,() =>{
     console.log(`server is running on port ${port}`);
 });
