4995242ad90679b830bc9218e07fab97cd9b1998
Amin Bm <medaminbenmbarek@gmail.com>
2018-04-04 12:34:02 +0100

    complete the fundamentals of chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,25 @@
 var socket =io();
 socket.on('connect',()=>{
     console.log('Connected to the server');
-
 });
 
 socket.on('newMessage',function (message) {
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
     console.log('new message comes',message);
 });
 
 socket.on('disconnect',()=>{
     console.log('Disconnected from the server');
+});
+
+jQuery('#message-form').on('submit',function (e) {
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from :'User',
+        text : jQuery('[name=message]').val()
+    },function (data) {
+        console.log('Got',data)
+    });
 });
--- server/server.js
+++ server/server.js
@@ -4,25 +4,26 @@ const http =require('http');
 const express =require('express');
 const socketIO =require('socket.io');
 
-
 var port =process.env.PORT || 3000;
 var app =express();
 var server= http.createServer(app);
 var io=socketIO(server);
+var {generateMessage} =require('./../utils/message');
 
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log("A new client id connected");
 
-    socket.on('createMessage',(message)=> {
+    socket.emit('newMessage',generateMessage("Admin","Welcome to the application chat"));
+    //socket.brodcast.emit : sends the messages for all the users exept the one that sends the message
+    socket.broadcast.emit('newMessage',generateMessage("Admin","New user joinded the app"));
+
+    socket.on('createMessage',(message,callback)=> {
        console.log('Email created',message);
        /*on.emit sends the data for every single connection | socket.emit sends the data for a single connection */
-    io.emit('newMessage',{
-    from:message.from,
-    text :message.text,
-    createdAt :new Date().getTime()
-});
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('Acknowledgment from the server');
     });
 
     socket.on('disconnect',()=>{
